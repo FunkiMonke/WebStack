d15ab416aeed6cf80f5bd7728bfb242a467d825d
FunkiMonke <imranz.w01@gmail.com>
2026-01-19 19:58:09 +0000

    week1 done!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getNumbers()
 {
     const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
     return {num1, num2};
 }
 
@@ -9,24 +9,38 @@ function calculate(operation)
 {
     const {num1, num2} = getNumbers();
     let result;
+    let op;
     switch(operation)
     {
         case "add":
             result = num1 + num2;
+            op = "+";
+            history(num1, num2, op, result);
             break;
         case "subtract":
             result = num1 -num2;
+            op = "-";
             break;
         case "multiply":
             result = num1 * num2;
+            op = "*";
             break;
         case "divide":
             result = num2 !== 0 ? num1/num2 : "can't divide 0";
+            op = "/";
             break;
         default:
             result = "dumbdumb";
     }
-    return result;
+    return result;  
+}
+
+function history(num1, num2, operation, result)
+{
+    const entry = document.createElement("li");
+    entry.innerText = `${num1} ${operation} ${num2} = ${result}`;
+    document.getElementById('history').appendChild(entry);
+
 }
 
 document.getElementById('add').addEventListener('click', () => {
@@ -44,3 +58,9 @@ document.getElementById('multiply').addEventListener('click', () => {
 document.getElementById('divide').addEventListener('click', () => {
     document.getElementById('result').textContent = `Result: ${calculate('divide')}`;
 });
+
+document.getElementById('clear').addEventListener('click', ()=>{
+    document.getElementById('num1').value = '';
+    document.getElementById('num2').value = '';
+    document.getElementById('result').textContent = "fuck";
+})
