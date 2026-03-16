84b1381c39659da765e1ebf22cdced6e384e832f
FunkiMonke <imranz.w01@gmail.com>
2026-03-16 14:26:34 +0000

    week9 lab sheet ACTUALLY WORKING YIPEEEPEPEPEPE

--- week9/app.py
+++ week9/app.py
@@ -1,19 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import flask_sqlalchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////workspace/database.db'
-db = SQLAlchemy(app)
+# Configure the SQLite database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/imran/Desktop/School Work/WebStack/week9/database.db'
+db = flask_sqlalchemy.SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return "yo"
+    return "Welcome to the F1 Team Management System!"
 
+# Ensure database tables are created
 def create_tables():
     with app.app_context():
         db.create_all()
-        print(f"Database created at {db_path}")
+        print(f"Database created at ")
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
