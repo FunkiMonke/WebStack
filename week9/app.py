8feb1b103364ef86e409a361c727dbb8c8d58a8b
FunkiMonke <imranz.w01@gmail.com>
2026-03-16 11:48:20 +0000

    comit lol

---
+++ week9/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////workspace/database.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "yo"
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+        print(f"Database created at {db_path}")
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8000)
